a1763108f59634b02bbb6112d8bf631851a64489
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-26 20:59:21 +0900

    snippets: ExpertPython pp.72-73 Other idioms. #2

--- ExpertPython/02.SyntaxBestPracticeExceptClass/snippets.py
+++ ExpertPython/02.SyntaxBestPracticeExceptClass/snippets.py
@@ -21,3 +21,25 @@ for i in range(10):
 
 # Elegant
 [i for i in range(10) if i % 2 == 0]
+
+'''
+Other idioms
+'''
+
+i = 0
+for element in ['one', 'two', 'three']:
+    print(i, element)
+    i += 1
+
+# enumerate
+for i, element in enumerate(['one', 'two', 'three']):
+    print(i, element)
+
+# zip
+for item in zip(*zip([1, 2, 3], [4, 5, 6])):
+    print(item)
+
+# unpack
+first, second, *rest = 0, 1, 2, 3
+first, *inner, last = 0, 1, 2, 3
+(a, b), (c, d) = (1, 2), (3, 4)
