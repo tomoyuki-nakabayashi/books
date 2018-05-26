383c0b05c8623d116c4d293efe3343ffc35bd32a
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-27 08:41:20 +0900

    snippets: ExpertPython pp.81-85 generator. #2

--- ExpertPython/02.SyntaxBestPracticeExceptClass/high_level_syntax.py
+++ ExpertPython/02.SyntaxBestPracticeExceptClass/high_level_syntax.py
@@ -13,3 +13,62 @@ class CountDown:
     def __iter__(self):
         """Return the iterator itself."""
         return self
+
+'''
+Generator
+'''
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        yield b
+        a, b = b, a + b
+
+# How to use
+fib = fibonacci()
+next(fib)
+next(fib)
+[next(fib) for i in range(10)]
+
+def power(values):
+    for value in values:
+        print('Supply %s.', value)
+        yield value
+
+def adder(values):
+    for value in values:
+        print('Add a value to %s', value)
+        if value % 2 == 0:
+            yield value + 3
+        else:
+            yield value + 2
+
+# How to use
+elements = [1, 4, 7, 9, 12, 19]
+results = adder(power(elements))
+next(results)
+next(results)
+
+def psychologist():
+    print('What is your concern?')
+    while True:
+        answer = (yield)
+        if answer is not None:
+            if answer.endswith('?'):
+                print("You should not ask for yourself.")
+            elif 'good' in answer:
+                print("Sounds good! Do it!")
+            elif 'bad' in answer:
+                print("Don't mind")
+
+# How to use
+free = psychologist
+next(free)
+free.send('I feel bad.')
+
+'''
+Generator expression
+'''
+
+iter = (x**2 for x in range(10) if x % 2 == 0)
+for el in iter:
+    print(el)
