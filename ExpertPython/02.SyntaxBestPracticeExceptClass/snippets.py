7a4de4db55a068f94fdac2d3586b733625379b68
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-26 20:50:58 +0900

    snippets: ExpertPython pp.71 List comprehension. #2

--- ExpertPython/02.SyntaxBestPracticeExceptClass/snippets.py
+++ ExpertPython/02.SyntaxBestPracticeExceptClass/snippets.py
@@ -1,3 +1,6 @@
+'''
+Concat strings.
+'''
 s = ""
 # ineffective
 for substring in substrings:
@@ -5,3 +8,16 @@ for substring in substrings:
 
 # more effective
 s = "".join(substrings)
+
+'''
+List comprehension
+'''
+
+# ugly
+evens = []
+for i in range(10):
+    if i % 2 == 0:
+        evens.append(i)
+
+# Elegant
+[i for i in range(10) if i % 2 == 0]
